261060fcc92aab22b16098e6144b2a864fb91167
MatosOliveira <matosalem.oliveira@gmail.com>
2023-06-13 18:22:26 -0300

    Projeto inicial

---
+++ src/model/entities/enums/OrderStatus.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package model.entities.enums;
+
+/**
+ * Enum - Status dos pedidos
+ * 
+ * @author Matos - 12.06.2023
+ *
+ */
+public enum OrderStatus {
+
+	PENDING_PAYMENT,
+	PROCESSING,
+	SHIPPED,
+	DELIVERED;
+}
